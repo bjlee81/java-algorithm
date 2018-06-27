e5a95e70439df64bf3429781af1235c6ea8b820c
bjlee81 <bjlee81@neighbor21.co.kr>
2018-06-27 14:13:46 +0900

    sort 모듈 의존성 추가

--- median-of-two-sorted-arrays/src/main/java/com/bjlee/median_of_two_sorted_arrays/App.java
+++ median-of-two-sorted-arrays/src/main/java/com/bjlee/median_of_two_sorted_arrays/App.java
@@ -1,6 +1,6 @@
-package com.bjlee81.median_of_two_sorted_arrays;
+package com.bjlee.median_of_two_sorted_arrays;
 
-import java.util.Arrays;
+import com.bjlee.sort.Sort;
 
 public class App {
 
@@ -8,9 +8,11 @@ public class App {
         Solution solution = new Solution();
         int[] nums1 = new int[]{1, 3, 5, 4, 8, 12};
         int[] nums2 = new int[]{2, 7, 6, 10, 11, 9};
+        Sort sort = new Sort();
+        sort.insertionSort(nums1);
+        sort.insertionSort(nums2);
         solution.findMedianSortedArrays(nums1, nums2);
 
-        solution.insertionSort(nums1);
     }
 }
 
@@ -26,8 +28,8 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
         // 정렬되지 않았으면 정렬을 수행
-//        sortArray(nums1, nums1[0], nums1[nums1.length-1]);
-//        sortArray(nums2, nums2[0], nums2[nums2.length-1]);
+//        quickSort(nums1, nums1[0], nums1[nums1.length-1]);
+//        quickSort(nums2, nums2[0], nums2[nums2.length-1]);
 
         // 두개의 배열을 합하고 sorted array merge 한 후 중간의 두값에 대해 2등분하면 될듯??
         // 이미 sorted 된 값이 기제..
@@ -77,36 +79,5 @@ class Solution {
         }
 
         return (arr[tSize / 2] + arr[(tSize / 2) - 1]) / 2.0;
-    }
-
-    private void sortArray(int[] ints, int left, int right) {
-        // 퀵정렬??
-        int i, j, pivot, tmp;
-        pivot = ints[ints.length / 2];
-
-        for (int k = 1; k < ints.length; k++) {
-            int curV = ints[k];
-            int preV = k - 1;
-
-            if (ints[preV] > curV) {
-                ints[k] = ints[preV];
-                preV--;
-            }
-        }
-    }
-
-    // 삽입정렬
-    void insertionSort(int[] arr) {
-        for (int index = 1; index < arr.length; index++) {
-            int temp = arr[index];
-            int aux = index - 1;
-
-            while ((aux >= 0) && (arr[aux] > temp)) {
-
-                arr[aux + 1] = arr[aux];
-                aux--;
-            }
-            arr[aux + 1] = temp;
-        }
     }
 }
